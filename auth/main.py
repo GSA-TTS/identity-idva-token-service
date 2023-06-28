f48b1aa2f34b8a3d75b116eb291cc7e7bac776b3
Peter Shumate <shumatepfs@gmail.com>
2023-06-28 11:30:27 -0400

    Fix request body parsing and validation (#20)

--- auth/main.py
+++ auth/main.py
@@ -11,9 +11,10 @@ app.config.from_object(app_settings)
 config = app.config
 db = SQLAlchemy(app)
 
-from auth.api import auth_blueprint
+from auth.api import auth_blueprint, gdrive_blueprint
 
 app.register_blueprint(auth_blueprint, url_prefix="/auth")
+app.register_blueprint(gdrive_blueprint, url_prefix="/export")
 
 with app.app_context():
     db.drop_all()
